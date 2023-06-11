33bb1c49078088f7f5809aff2b0f0a41cea254f1
Jebbril <orakib@student.1337.ma>
2023-06-11 19:49:04 +0100

    fixed cd segfault

--- builtins/m_cd.c
+++ builtins/m_cd.c
@@ -6,7 +6,7 @@
 /*   By: orakib <orakib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 11:02:46 by orakib            #+#    #+#             */
-/*   Updated: 2023/06/11 18:22:38 by orakib           ###   ########.fr       */
+/*   Updated: 2023/06/11 19:45:47 by orakib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ t_env	*get_envnode(t_env **envar, char *str)
 	return (NULL);
 }
 
-void	change_pwdvar(t_env **envar)
+int	change_pwdvar(t_env **envar)
 {
 	t_env	*pwd;
 	t_env	*oldpwd;
@@ -39,18 +39,21 @@ void	change_pwdvar(t_env **envar)
 	if (!str)
 	{
 		perror("Minishell");
-		return ;
+		return (1);
 	}
 	oldpwd = get_envnode(envar, "OLDPWD");
 	pwd = get_envnode(envar, "PWD");
+	if (!oldpwd || !pwd)
+		return (write(2, "cd: OLDPWD not set\n", 20), 1);
 	free(oldpwd->value);
 	oldpwd->value = pwd->value;
 	if (!getcwd(str, MAXPATHLEN))
 	{
 		free (str);
-		return ;
+		return (1);
 	}
 	pwd->value = str;
+	return (0);
 }
 
 int	change_dir(char *arg, t_env **envar)
@@ -58,6 +61,8 @@ int	change_dir(char *arg, t_env **envar)
 	t_env	*oldpwd;
 
 	oldpwd = get_envnode(envar, "OLDPWD");
+	if (!oldpwd)
+		return (write(2, "cd: OLDPWD not set\n", 20), 1);
 	if (strncmp(arg, "-", ft_strlen(arg)) == 0)
 	{
 		if (chdir(oldpwd->value) != 0)
@@ -65,7 +70,7 @@ int	change_dir(char *arg, t_env **envar)
 			perror("Minishell: cd");
 			return (EXIT_FAILURE);
 		}
-		change_pwdvar(envar);
+		return (change_pwdvar(envar));
 	}
 	else
 	{
@@ -74,7 +79,7 @@ int	change_dir(char *arg, t_env **envar)
 			perror("Minishell: cd");
 			return (EXIT_FAILURE);
 		}
-		change_pwdvar(envar);
+		return (change_pwdvar(envar));
 	}
 	return (EXIT_SUCCESS);
 }
@@ -92,14 +97,11 @@ int	m_cd(char **args, t_env **envar)
 			perror("Minishell: cd");
 			return (EXIT_FAILURE);
 		}
-		change_pwdvar(envar);
-		return (EXIT_SUCCESS);
+		return (change_pwdvar(envar));
 	}
 	if (!args[1][0])
 		return (EXIT_SUCCESS);
 	if (args[1])
-	{
-		change_dir(args[1], envar);
-	}
+		return (change_dir(args[1], envar));
 	return (EXIT_SUCCESS);
 }
